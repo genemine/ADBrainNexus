ad9857ab21157f4484c5f8edf502c20ae3c89d4a
GeneMine <48688298+genemine@users.noreply.github.com>
2021-10-23 17:12:29 +0800

    Update AD_gene_pred.py

--- AD_gene_pred.py
+++ AD_gene_pred.py
@@ -9,37 +9,16 @@ Created on Sat Mar 13 11:45:09 2021
 
 import numpy as np
 import pandas as pd
-import random,sys
-from sklearn.ensemble import  ExtraTreesClassifier,RandomForestClassifier
+import random,sys,os
 from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import roc_auc_score,roc_curve,average_precision_score,precision_recall_curve
-from sklearn import svm 
-from sklearn.linear_model import LogisticRegression 
+from sklearn.linear_model import RidgeCV
 
 #### input and output settings
-script,featurematrix,outputfileprefix,totaltimes=sys.argv
+script,out,featurematrix,totaltimes=sys.argv
 
-totaltimes=int(totaltimes)
-allnegagenes ='./data/negative.txt'
-posigenes='./data/gene.txt'
-CV_file=outputfileprefix+'_5folds_CV'+'.tsv'
-
-#preprocessing
-data=pd.read_csv(featurematrix,index_col=0,header=0,sep='\t')
-data.index=[str(indexi) for indexi in data.index]
-data=data.abs()
-columns=data.loc[(data.std(axis=1)<0.01),:].index.intersection(data.columns)
-data=data.drop(columns,axis=1)
-indexs=data.loc[(data.std(axis=1)>0.01),:].index
-data=data.loc[indexs,:]
-data=data.dropna()
-data = data[~data.index.duplicated(keep = "first")]
-models={}
-models['ExtraTrees']=ExtraTreesClassifier(n_estimators = 500,class_weight = "balanced_subsample", n_jobs = -1, bootstrap = True) 
-#models['RandomForest']=RandomForestClassifier(n_estimators = 500,class_weight = "balanced_subsample", n_jobs = -1, bootstrap = True) 
-#models['SVM']=svm.SVC(kernel = "linear",probability = True,class_weight = "balanced",C=0.01,gamma=100)
-#models['LogisticRegression']=LogisticRegression(solver='lbfgs',multi_class='multinomial',class_weight={0:0.5, 1:0.5})
-
+model=RidgeCV(alphas=[0.01, 0.1, 1, 10, 20, 30])
+os.system('mkdir '+out+'/')
 ############# below DO NOT change  ##################
 def to_list(filex):
     f=open(filex)
@@ -53,18 +32,15 @@ def to_list(filex):
 
 ### 5-fold CV
 print('5-fold CV')
-def kfoldcv(data,CV_file,gene):
-    AUROC={}
-    AUPRC={}
+def kfoldcv(data,CV_file,gene,negs):
+    random.seed(0)
     score={}
-    for modeli in models:
-        AUROC[modeli]=[]
-        AUPRC[modeli]=[]
+    count={}
+    AUROC=[]
+    AUPRC=[]
     for i in range(totaltimes):
         print('step:'+str(i))
-        nposigenes= len(gene)
-        negs=to_list(allnegagenes)
-        negs=list(data.index.intersection(negs))
+        nposigenes= len(gene) 
         negative=random.sample(list(negs),len(negs))
         index=gene+negative
         y=[1]*nposigenes+[0]*len(negative)
@@ -72,58 +48,67 @@ def kfoldcv(data,CV_file,gene):
         y.index=index
         X=data.loc[index,:]
         skf = StratifiedKFold(n_splits=5)
-        for modeli in models:
-            y_pred=pd.Series([])
-            y_new=pd.Series([])
-            for train,test in skf.split(X,y):
-                X_train, X_test = X.iloc[train,:], X.iloc[test,:]
-                y_train, y_test = y.iloc[train], y.iloc[test]
-                model=models[modeli]
-                model.fit(X_train,y_train)
-                pred=model.predict_proba(X_test)[:,1]
-                y_predi=pd.Series(pred)
-                y_predi.index=y_test.index
-                y_new=y_new.append(y_test)
-                y_pred=y_pred.append(y_predi)
-                left=data.index.difference(X_train.index)
-                X_left=data.loc[left,:]
-                P_left=model.predict_proba(X_left)[:,1]
-                P_left=pd.Series(P_left)
-                P_left.index=left
-                for genei in P_left.index:
-                    if genei not in score:
-                        score[genei]=[P_left[genei]]
-                    else:
-                        score[genei].append(P_left[genei])
-            AUROC[modeli].append(round(roc_auc_score(y_new, y_pred),4))
-            AUPRC[modeli].append(round(average_precision_score(y_new, y_pred),4))
-            result=pd.DataFrame(y_pred)
-            result['label']=y_new
-            result.to_csv(outputfileprefix+'pred_aurc.txt',sep='\t')
-            print(modeli+':'+str(AUROC[modeli]))
-            print(modeli+':'+str(AUPRC[modeli]))
+        y_pred=pd.Series([],dtype='float')
+        y_new=pd.Series([],dtype='float')
+        for train,test in skf.split(X,y):
+            X_train, X_test = X.iloc[train,:], X.iloc[test,:]
+            y_train, y_test = y.iloc[train], y.iloc[test]
+            model.fit(X_train,y_train)
+            pred=model.predict(X_test)
+            y_predi=pd.Series(pred)
+            y_predi.index=y_test.index
+            y_new=y_new.append(y_test)
+            y_pred=y_pred.append(y_predi)
+            left=data.index.difference(X_train.index)
+            X_left=data.loc[left,:]
+            Pred_left=model.predict(X_left)
+            Pred_left=pd.Series(Pred_left)
+            Pred_left.index=left
+            for genei in Pred_left.index:
+                if genei not in score:
+                    score[genei]=Pred_left[genei]
+                    count[genei]=1
+                else:
+                    score[genei]=score[genei]+Pred_left[genei]
+                    count[genei]=count[genei]+1
+        AUROCi=round(roc_auc_score(y_new, y_pred),4)
+        AUPRCi=round(average_precision_score(y_new, y_pred),4)
+        print('AUROC:'+str(AUROCi))
+        print('AUPRC:'+str(AUPRCi))
+        AUROC.append(AUROCi)
+        AUPRC.append(AUPRCi)
     fnew=open(CV_file,'w')
     result={}
     result['AUPRC']=AUPRC
     result['AUROC']=AUROC
     fnew.write(str(result))
     fnew.close()
-    ave={}
-    ave=pd.Series(ave)
-    ave=ave.sort_values(ascending=False).round(4)
-    ave.to_csv(outputfileprefix+'_pred.txt',sep='\t')
+    result={}
+    for genei in data.index:
+        if genei in score:
+            result[genei]=score[genei]/count[genei]
+    x=pd.Series(result)
+    x=x.sort_values(ascending=False)
+    x.to_csv(out+'/prediction.txt',sep='\t')
 
 
-gene=list(set(data.index.intersection(to_list(posigenes))))
-print(len(gene))
+###main####
+        
+totaltimes=int(totaltimes)
+posigenes=to_list('data/positive.txt')
+negs=to_list('data/negative.txt')
+CV_file=out+'/5folds_CV'+'.tsv'
+
+
+#preprocessing
+data=pd.read_csv(featurematrix,index_col=0,header=0,sep='\t')
+data.index=[str(indexi) for indexi in data.index]
+data=data.dropna()
+data = data[~data.index.duplicated(keep = "first")]
+gene=list(set(data.index.intersection(data.columns.intersection(posigenes))))
+negs=list(set(data.index.intersection(negs)).difference(gene))
+
+
 #model performance
-
-kfoldcv(data,CV_file,gene)
-
+kfoldcv(data,CV_file,gene,negs)
 # end of 5-fold CV
-
-
-
-
-
-
